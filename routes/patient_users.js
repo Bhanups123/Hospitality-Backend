8563fe95e7e7f592bd5a69f5c28adad7b632b92a
Bhanu Pratap Singh <sbhanupratap161@gmail.com>
2020-04-12 03:40:20 +0530

    few refactoring

--- routes/patient_users.js
+++ routes/patient_users.js
@@ -7,34 +7,7 @@ const keys = require("../config/keys");
 const passport = require("passport");
 const router = express.Router();
 
-//degree to radians
-const toRadians = (degree) => {
-  const one_deg = 22 / (7 * 180);
-  return one_deg * degree;
-};
-
-//distance calculator fn
-const distCalc = (lat1, long1, lat2, long2) => {
-  lat1 = toRadians(lat1);
-  long1 = toRadians(long1);
-  lat2 = toRadians(lat2);
-  long2 = toRadians(long2);
-
-  const dlong = long2 - long1;
-  const dlat = lat2 - lat1;
-
-  let ans =
-    Math.pow(Math.sin(dlat / 2), 2) +
-    Math.cos(lat1) * Math.cos(lat2) * Math.pow(Math.sin(dlong / 2), 2);
-
-  ans = 2 * Math.asin(Math.sqrt(ans));
-
-  const R = 6371;
-
-  ans = ans * R;
-
-  return ans;
-};
+const distCalc = require("../utils/distCalc");
 
 //nearby hospitals route
 router.get("/hospitals", (req, res) => {
