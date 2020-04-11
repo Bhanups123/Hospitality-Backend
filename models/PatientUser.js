07c55ae8cb532851519e8fc56d9239f791606e4e
Bhanu Pratap Singh <sbhanupratap161@gmail.com>
2020-04-12 02:26:16 +0530

    created patient schema

---
+++ models/PatientUser.js
@@ -0,0 +1,35 @@
+const mongoose = require("mongoose");
+
+const patientUserSchema = mongoose.Schema({
+  name: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  contact: {
+    type: Number,
+    required: true,
+  },
+  latitude: {
+    type: Number,
+    required: true,
+  },
+  longitude: {
+    type: Number,
+    required: true,
+  },
+});
+
+module.exports = PatientUser = mongoose.model("PatientUser", patientUserSchema);
