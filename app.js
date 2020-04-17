b7c495c559ef1059c96435769cf29af52284b9ce
Bhanu Pratap Singh <sbhanupratap161@gmail.com>
2020-04-17 12:48:30 +0530

    modified def header

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const app = express();
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 app.use((req, res, next) => {
-  res.header("Content-Type", "application/json");
+  req.header("Content-Type", "application/json");
   next();
 });
 //db config
