bbdb266b0e74ea623a9b0a5a60744ae3fe1dac9e
Bhanu Pratap Singh <sbhanupratap161@gmail.com>
2020-04-19 22:31:48 +0530

    added mailgun setup keys temporarily

--- config/keys.js
+++ config/keys.js
@@ -2,4 +2,6 @@ module.exports = {
   mongoURI:
     "mongodb+srv://bhanu:hack_bhanu@cluster0-jjinl.gcp.mongodb.net/test?retryWrites=true&w=majority",
   secretOrKey: "bhaagbc",
+  MJ_APIKEY_PUBLIC: "0e091d0bb97ad374fd92f46d6dc9a9b8",
+  MJ_APIKEY_PRIVATE: "423998c6b4564ae16c644f3e7863b051",
 };
