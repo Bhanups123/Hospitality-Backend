af5caf0ea7fd290b82053d95e2deb73ba40ccd86
Bhanu Pratap Singh <sbhanupratap161@gmail.com>
2020-04-20 04:14:01 +0530

    proper name

---
+++ utils/sendVerifyCode.js
@@ -0,0 +1,37 @@
+const keys = require("../config/keys");
+const getRandCode = require("./getRandCode");
+const User = require("../models/User");
+
+//connecting mailgun
+var mailgun = require("mailgun-js")({
+  apiKey: keys.API_KEY,
+  domain: keys.DOMAIN,
+});
+
+module.exports = (email, userType) => {
+  const code = getRandCode();
+
+  const data = {
+    from: "helpdesk@hospitality.com",
+    to: email,
+    subject: "Account Verification!!",
+    html: `<h1>Hospitality</h1><br><h3>Welcome To Hospitality Service!! Get Right Info! Right Time!</h3><br><p>You need to verify your account by entering below code:</p><br> <b>${code}</b><br><p>Thank You! Apka Din mangal me hoichh!!</p>`,
+  };
+
+  mailgun.messages().send(data, (error, body) => {
+    console.log(body);
+  });
+
+  User.findOne({ email })
+    .then((user) => {
+      if (!user) {
+        User.create({ email, code, userType }).then((user_s) =>
+          console.log(user_s)
+        );
+      } else {
+        user.code = code;
+        user.save();
+      }
+    })
+    .catch();
+};
