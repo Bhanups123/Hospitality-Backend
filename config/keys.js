2bd6d08bdd2c9c1cd94a235f2b7aac318f80a292
Bhanu Pratap Singh <44453307+Bhanups123@users.noreply.github.com>
2020-04-20 01:45:56 +0530

    Delete keys.js

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://bhanu:hack_bhanu@cluster0-jjinl.gcp.mongodb.net/test?retryWrites=true&w=majority",
-  secretOrKey: "bhaagbc",
-  API_KEY: "1d55348f36f3ce9f27409d346adaf03e-915161b7-c5cd6f9c",
-  DOMAIN: "sandboxb050d428a02c41d3bf0a0919caae2910.mailgun.org",
-};
